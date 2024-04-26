54aae1d8c3a29343a7ab6440e993f29e34c8ef3d
ChoiMari <chl98639863@gmail.com>
2024-04-26 18:33:18 +0900

    20240426

--- lab_sql/sql10.sql
+++ lab_sql/sql10.sql
@@ -7,3 +7,29 @@ create table blogs(
     modified_time   timestamp           default systimestamp,
     constraint blogs_pk primary key (id)
 );
+
+insert into blogs (title, content, writer)
+values ('안녕하세요','첫번째 블로그 테스트','admin'); --id 프라이머리키 : insert시 반드시 값을 넣어야함 안넣으면 제약조건 위배
+--근데 성공함 이유: generated always as identity 해놓아서 insert할때마다 항상 자동으로 만들어 준다고
+-- insert할 때마다 숫자들 1씩 자동으로 증가  generated always as identity
+--주의. 오라클 12버전부터 가능한 문법.
+--시간 자동으로 들어감 default로 설정해놓아서
+insert into blogs (title, content, writer)
+values ('test','두번째 블로그 테스트','guest');
+
+select * from blogs;
+
+commit;
+
+
+
+
+
+
+
+
+
+
+
+
+
