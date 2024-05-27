3202ff3c2bfff6258f5d6aee2d1913b2c4f3a486
ChoiMari <chl98639863@gmail.com>
2024-05-27 15:30:10 +0900

    20240527

---
+++ lab05_jsp/src/test/java/com/itwill/lab05/datasource/HikariDataSourceTest.java
@@ -0,0 +1,14 @@
+package com.itwill.lab05.datasource;
+
+import org.junit.jupiter.api.Test;
+
+public class HikariDataSourceTest {
+	
+	private final DataSourceUtil util = DataSourceUtil.getInstance();
+	
+	@Test
+	public void test() {
+		System.out.println("test...");
+	}
+
+}
